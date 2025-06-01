db7cd37f3a9a2e0aedd89c376a7b7e4c87664af6
Aaron <niceboy@duck.com>
2025-06-01 09:13:20 +0800

    ci(workflow): 添加OpenWrt构建工作流及自定义脚本
    添加GitHub Actions工作流文件用于自动化构建OpenWrt固件
    包含自定义脚本修改默认IP地址和主题

---
+++ diy.sh
@@ -0,0 +1,5 @@
+# Modify default IP
+sed -i 's/192.168.6.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
